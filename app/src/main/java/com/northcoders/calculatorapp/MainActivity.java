3d45c13750b0f57c99632ce779e07c814b3d97f7
ag-af <agabialves11@gmail.com>
2024-09-12 15:40:59 +0100

    feat: Implement MainActivity methods.

--- app/src/main/java/com/northcoders/calculatorapp/MainActivity.java
+++ app/src/main/java/com/northcoders/calculatorapp/MainActivity.java
@@ -1,6 +1,11 @@
 package com.northcoders.calculatorapp;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +13,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText firstNumberInput;
+    EditText secondNumberInput;
+    Button addButton;
+    TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,32 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        firstNumberInput = findViewById(R.id.firstNumberInput);
+        secondNumberInput = findViewById(R.id.secondNumberInput);
+        addButton = findViewById(R.id.addButton);
+        result = findViewById(R.id.result);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String firstNumber = firstNumberInput.getText().toString();
+                String secondNumber = secondNumberInput.getText().toString();
+
+                if (!firstNumber.isEmpty() && !secondNumber.isEmpty()) {
+                    int firstNumberInt = Integer.parseInt(firstNumber);
+                    int secondNumberInt = Integer.parseInt(secondNumber);
+
+                    int resultInt = firstNumberInt + secondNumberInt;
+
+                    result.setText(String.valueOf(resultInt));
+//                    Log.i("Result: ", result);
+                } else {
+                    result.setText("Please enter two numbers");
+                }
+            }
+        });
     }
+
+
 }
